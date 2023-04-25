483f02d65426542711c84b360681cf436b2c6f01
Osama-Youssef <osama0.syria@gmail.com>
2023-04-25 12:42:43 +0300

    this seconde edit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h2>osama</h2>
+      <h3>samgi</h3>
+      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        click me
+      </button>
+    </>
   );
 }
 
