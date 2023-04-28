21e27be26333b8f1566eb411e682acc9c4bd0b31
Osama-Youssef <osama0.syria@gmail.com>
2023-04-28 23:08:23 +0300

    here there is a problem in fun regular Expressions that  it is mapping every time i want mapping it just one time

--- src/compoent/Section.js
+++ src/compoent/Section.js
@@ -13,6 +13,22 @@ function Section() {
     return age;
   };
 
+  let regularExpressions = (text) => {
+    const moudelWords =
+      /html\d? |css|js|java script|javascript|sass|react.js|python|ts|type script|next.js|mysql/gim;
+    const matchmoudel = text.match(moudelWords);
+    // return text.replaceAll(moudelWords,`<span className="text-red-300">${moudelWords}</span>`)
+    let resault = matchmoudel.map((ele) => {
+      let varibal= text.replace(
+        ele,
+        `<span className="text-red-300">${ele}</span>`
+        );
+       
+    });
+    console.log(matchmoudel);
+    return resault;
+  };
+
   let mapping = (arr) => {
     const resault = arr.map(function (item) {
       return (
@@ -20,17 +36,18 @@ function Section() {
           key={item.id}
           firstName={item.firstName}
           lastName={item.lastName}
+          age={getAge(item.age)}
+          jop={item.jop}
+          bio={regularExpressions(item.bio)}
           email={item.email}
           tel={item.tel}
           img={item.img}
-          age={getAge(item.age)}
         />
       );
     });
     return resault;
   };
 
-  console.log(`this Array`, mapping(InfoTeam));
   return (
     <section className="flex gap-4 flex-col  align-center items-center w-full h-fit p-4">
       {mapping(InfoTeam)}
