6548a1f2c60e1127ed89a09b8f1600dfefc6be7b
Osama-Youssef <osama0.syria@gmail.com>
2023-04-28 23:08:59 +0300

    no commit

--- src/compoent/Card.js
+++ src/compoent/Card.js
@@ -4,11 +4,15 @@ import React from "react";
 function Card(props) {
   return (
     <>
-      <div className="max-w-sm w-1/2 lg:max-w-full lg:flex">
+      <div className="max-w-lg- w-1/2 rounded-2xl border-2 border-sky-500 overflow-hidden lg:max-w-full lg:flex">
         <div
-          className="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden"
-          style={{backgroundImage: `url(${props.img})`}}
-          title="Woman holding a mug"
+          className={`
+            h-full w-full  rounded-t text-center overflow-hidden flex-none
+             bg-red-400 bg-cover  bg-center bg-no-repeat 
+             lg:h-auto lg:w-48 lg:rounded-l lg:rounded-t-none 
+            `}
+          style={{ backgroundImage: `url(${props.img})` }}
+          title={`${props.firstName} ${props.lastName}`}
         ></div>
         <div className="border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
           <div className="mb-8">
@@ -22,21 +26,26 @@ function Card(props) {
               </svg>
               Members only
             </p>
+
             <div className="text-gray-900 font-bold text-xl mb-2">
-              Can coffee make you a better developer?
+              {props.jop}
             </div>
-            <p className="text-gray-700 text-base">
-              Lorem ipsum dolor sit amet, consectetur adipisicing , nulla!
-              Maiores et perferendis eaque, exercitationem praesentium nihil.
-            </p>
+
+            <p className="text-gray-700 text-base">{props.bio}</p>
+            <div className=" flex flex-col justify-center text-sm ">
+              {props.tel !== null ? <a>TEL :{props.tel}</a> : ""}
+              {props.email !== null ? <a>Email : {props.email}</a> : ""}
+            </div>
           </div>
+
           <div className="flex items-center">
             <img
               className="w-10 h-10 rounded-full mr-4"
               src={props.img}
               alt="Avatar of Jonathan Reinink"
             />
-            <div className="text-sm">
+
+            <div className="text-sm ">
               <p className="text-gray-900 leading-none">
                 {props.firstName} {props.lastName}
               </p>
--- src/compoent/InfoTeam.js
+++ src/compoent/InfoTeam.js
@@ -1,29 +1,45 @@
 const InfoTeam = [
   {
-    id:"001osama",
+    id: "001osama",
     firstName: "osama",
     lastName: "youssef",
-    age:`2002 3 15` ,
+    age: `2002 3 15`,
+    jop: `  Front-End developer`,
+    bio: `Hi every one , 
+    I am a Front-End developer and I know
+     HTML5  CSS JS REACT.JS SASS NEXT.JS TYPE SCRIPT 
+    I have two years of experanc at this carrer 
+    call me if you want to professional man to make your websit `,
     email: "osama0.syria@gmail.com",
     tel: "+963951505235",
     img: "https://scontent-fra3-1.xx.fbcdn.net/v/t39.30808-6/301988622_1169503606929692_290917263758716719_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=lE-P5w7-U1YAX_52fR7&_nc_ht=scontent-fra3-1.xx&oh=00_AfBKXcCFE5m3aGZTMOSLrSWdmImNjRkluYPL-cBTImvRKw&oe=644C914E",
   },
 
   {
-    id:"002hamaz",
+    id: "002hamaz",
     firstName: "hamza",
     lastName: "dandan",
-    age:`"2002" "2" "15"` ,
+    age: `2002 2 15`,
+    jop: `  Front-End developer`,
+    bio: `Hi every one , 
+    I am a Front-End developer and I know HTML5  CSS JS REACT.JS SASS NEXT.JS TYPE SCRIOPT 
+    I have two years of experanc at this carrer 
+    call me if you want to professional man to make your websit `,
     email: "hamzon.aldandalon@gmail.com",
     tel: "+963981196047",
     img: "https://scontent-fra5-2.xx.fbcdn.net/v/t39.30808-6/287309093_1489760884827261_84828940403721883_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=FfYhKhAf1LsAX_WtA0u&_nc_ht=scontent-fra5-2.xx&oh=00_AfBKyqkduxt4mywHNxeyjam72-d_ZltDweVadhaFEY4wEA&oe=644CDF9A",
   },
 
   {
-    id:"003goorje",
+    id: "003goorje",
     firstName: "goorje",
     lastName: "alnajjar",
-    age:`2002 2 15` ,
+    age: `2002 2 15`,
+    jop: `  Back-End developer`,
+    bio: `Hi every one , 
+    I am a Back-End developer and I know HTML5  CSS PYTHON MYSQL  
+    I have two years of experanc at this carrer 
+    call me if you want to professional man to make your websit `,
     email: "georgenajjar77@gmail.com",
     tel: "+963938697252",
     img: "https://scontent-fra3-1.xx.fbcdn.net/v/t1.6435-1/37712134_985519354961700_6117695221070299136_n.jpg?stp=dst-jpg_p200x200&_nc_cat=104&ccb=1-7&_nc_sid=7206a8&_nc_ohc=xM3D2wnmKPoAX91zV8z&_nc_ht=scontent-fra3-1.xx&oh=00_AfDLsFVg_ePEuS4x0_b84mQkMw2bGeywHFKMTRj6R5gOfw&oe=646F7F1E",
